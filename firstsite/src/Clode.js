9dc9f5738ff9291b2492938e06ddda0b1bd15593
matizz1 <matvei2006golova@gmail.com>
2023-11-17 18:57:29 +0300

    11

--- firstsite/src/Clode.js
+++ firstsite/src/Clode.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TTTT2849 from './images/TTTT2849.jpg'
 import jopa from './images/jopa.jpg'
 import nejopa from './images/nejopa.jpg'
+import {Button} from 'react-bootstrap';
 
 const Clode = () => {
   return (
@@ -13,22 +14,31 @@ const Clode = () => {
         </div>  
           <div id="cards">   
             <div id="card1">
-              <p>
-                Дошкольники и младшая школа
-              </p> 
+              <div id="qwcard1">
+                <p>
+                  Дошкольники и младшая школа
+                </p> 
+                  <Button variant="success">Success</Button>{' '}
+              </div>
                 <img id="imgvg" src={TTTT2849} alt=''/> 
             </div>
             <div id="card2">
-                <img id="imgvg" src={jopa} alt=''/>
-              <p>
-                Средняя школа
-              </p>
+              <div id="qwcard2">
+                <p>
+                 Средняя школа
+                </p>
+                  <Button variant="success">Success</Button>{' '}
+              </div>
+                <img id="imgvg" src={jopa} alt=''/> 
             </div>
             <div id="card3">
-              <img id="imgvg" src={nejopa} alt=''/>
-                <p>
-                 Старшая школа и взрослые
-                </p>
+              <div id="qwcard3">
+                  <p>
+                   Старшая школа и взрослые
+                  </p>
+                    <Button variant="success">Success</Button>{' '}
+              </div>
+                <img id="imgvg" src={nejopa} alt=''/>
             </div>
 
           
